e93a81803bb8abbd3554780699ae94d60990bc1d
fujing <156163947@qq.com>
2024-07-15 17:39:19 +0800

    fix 调整项目目录

---
+++ Javascript/1.斐波拉契数列/index.js
@@ -0,0 +1,17 @@
+
+function draw() {
+  drawImage();
+}
+
+function drawImage() {
+  // 检查支持性
+  const canvas = document.getElementById("canvas");
+
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+    // 这里就可以写你想要的代码啦~~~
+    
+  } else {
+    alert("你的浏览器不支持 canvas，请更换浏览器再使用。");
+  }
+}
